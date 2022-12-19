5c40b4136f78d289d409bc3618451a8067641f35
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-12-19 19:00:38 +0100

    Hello world!

---
+++ src/main/java/nl/jordyvanvorselen/ribreviews/RibReviewsApplication.java
@@ -0,0 +1,11 @@
+package nl.jordyvanvorselen.ribreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibReviewsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RibReviewsApplication.class, args);
+	}
+}
---
+++ src/test/java/nl/jordyvanvorselen/ribreviews/RibReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.jordyvanvorselen.ribreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
