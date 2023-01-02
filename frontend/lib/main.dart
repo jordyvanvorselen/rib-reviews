9e76744b1ad65dcbb60e5e7b206f5c56718b64f3
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2023-01-02 21:47:19 +0100

    Add fonts and theme

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'utils/constants.dart';
+
 void main() {
-  runApp(Main());
+  runApp(const Main());
 }
 
 class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: "Rib Reviews",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context)
+            .textTheme
+            .apply(bodyColor: kPrimaryTextColor, fontFamily: 'Onest'),
+        iconTheme: const IconThemeData(color: kPrimaryTextColor),
+      ),
+    );
   }
 }
