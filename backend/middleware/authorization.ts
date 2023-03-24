3567ae28b8dd08f0d9215491ce9de235992c0c54
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2023-03-24 21:21:53 +0100

    Use google auth in authorization middleware

--- backend/middleware/authorization.ts
+++ backend/middleware/authorization.ts
@@ -1,14 +1,32 @@
+import { OAuth2Client } from "google-auth-library";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Middleware } from "next-api-route-middleware";
+
+const CLIENT_ID = "";
+const client = new OAuth2Client(CLIENT_ID);
 
 export const authorize: Middleware<NextApiRequest> = async (
   req: NextApiRequest,
   res: NextApiResponse,
   next: Function
 ) => {
-  if (req.headers.authorization === process.env.API_KEY) {
-    return next();
-  }
+  if (!req.headers.authorization) return unauthorized(res);
 
-  return res.status(401).send({ message: "Unauthorized" });
+  const ticket = await client.verifyIdToken({
+    idToken: req.headers.authorization,
+    audience: CLIENT_ID,
+  });
+
+  const payload = ticket.getPayload();
+
+  if (!payload) return unauthorized(res);
+
+  const domain = payload["hd"];
+
+  if (domain !== "@kabisa.nl") return unauthorized(res);
+
+  return next();
 };
+
+const unauthorized = (res: NextApiResponse) =>
+  res.status(401).send({ message: "Unauthorized" });
