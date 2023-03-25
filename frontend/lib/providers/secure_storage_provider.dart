939c33647510cfa65c0f2dd4fc6e5c7b4632bdf7
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2023-03-25 13:13:15 +0100

    Remove dead code

--- frontend/lib/providers/secure_storage_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
-class SecureStorageProvider with ChangeNotifier {
-  final storage = const FlutterSecureStorage();
-}
