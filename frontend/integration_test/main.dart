14d6433bfc3de56b2a565da78f7921790789698a
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2023-05-22 01:56:59 +0200

    Re-enable tests

--- frontend/integration_test/main.dart
+++ frontend/integration_test/main.dart
@@ -1,8 +1,8 @@
 import 'package:integration_test/integration_test.dart';
 
-// import 'home_screen_feature.dart' as home_screen;
-// import 'login_screen_feature.dart' as login_screen;
-// import 'review_modal_feature.dart' as review_modal;
+import 'home_screen_feature.dart' as home_screen;
+import 'login_screen_feature.dart' as login_screen;
+import 'review_modal_feature.dart' as review_modal;
 import 'review_screen_feature.dart' as review_screen;
 
 void main() {
@@ -10,8 +10,8 @@ void main() {
 
   binding.testTextInput.register();
 
-  // login_screen.main();
-  // home_screen.main();
+  login_screen.main();
+  home_screen.main();
   review_screen.main();
-  // review_modal.main();
+  review_modal.main();
 }
