05fea34f3f54cbbc316cbb1c736ec8183d90c51f
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2023-05-26 15:18:37 +0200

    Use typescript

--- slackbot/src/app.ts
+++ slackbot/src/app.ts
@@ -2,10 +2,11 @@ const { App } = require("@slack/bolt");
 
 require("dotenv").config();
 
-// Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
 });
 
 (async () => {
