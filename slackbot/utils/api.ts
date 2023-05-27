f29e822ee4a3ae5a422263ae05bce59311fb002c
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2023-05-27 16:19:42 +0200

    Remove double /api for POST as well

--- slackbot/utils/api.ts
+++ slackbot/utils/api.ts
@@ -11,15 +11,11 @@ export const get = async (url: string) => {
 };
 
 export const post = async (url: string, body: object | undefined) => {
-  return axios.post(
-    `${process.env.API_BASE_URL}/api${url}`,
-    JSON.stringify(body),
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        Authorization: process.env.API_KEY,
-      },
-    }
-  );
+  return axios.post(`${process.env.API_BASE_URL}${url}`, JSON.stringify(body), {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: process.env.API_KEY,
+    },
+  });
 };
