f978abacc131558a49106a71c36d1ec3f8c4ab3a
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2023-05-27 15:00:52 +0200

    revert

--- slackbot/pages/api/slack/events.ts
+++ slackbot/pages/api/slack/events.ts
@@ -7,7 +7,10 @@ export const config = {
   },
 };
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "POST") {
     res
       .status(405)
