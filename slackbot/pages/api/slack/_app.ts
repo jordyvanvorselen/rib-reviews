85a0cb2b949ed085919c79128b082f7943ca0c27
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2023-05-27 15:23:35 +0200

    Add /suggest command

--- slackbot/pages/api/slack/_app.ts
+++ slackbot/pages/api/slack/_app.ts
@@ -1,6 +1,7 @@
 import { AppRunner } from "@seratch_/bolt-http-runner";
 import { App, FileInstallationStore, LogLevel } from "@slack/bolt";
 import { FileStateStore } from "@slack/oauth";
+import * as api from "../../../utils/api";
 
 require("dotenv").config();
 
@@ -19,12 +20,92 @@ export const appRunner = new AppRunner({
 
 const app = new App(appRunner.appOptions());
 
-app.event("app_mention", async ({ say }) => {
-  await say("Hi there!");
+app.command("/suggest", async ({ client, ack, logger, body }: any) => {
+  try {
+    await client.views.open({
+      trigger_id: body.trigger_id,
+      view: {
+        type: "modal",
+        callback_id: "view_1",
+        title: {
+          type: "plain_text",
+          text: "Suggest a restaurant",
+        },
+        blocks: [
+          {
+            type: "input",
+            block_id: "nameInput",
+            label: {
+              type: "plain_text",
+              text: "Restaurant Name",
+            },
+            element: {
+              type: "plain_text_input",
+              action_id: "plain_input",
+              placeholder: {
+                type: "plain_text",
+                text: 'e.g. "Denver"',
+              },
+            },
+          },
+          {
+            type: "input",
+            block_id: "locationInput",
+            label: {
+              type: "plain_text",
+              text: "City",
+            },
+            element: {
+              type: "plain_text_input",
+              action_id: "plain_input",
+              placeholder: {
+                type: "plain_text",
+                text: 'e.g. "Weert"',
+              },
+            },
+          },
+          {
+            type: "input",
+            block_id: "websiteInput",
+            label: {
+              type: "plain_text",
+              text: "Website URL",
+            },
+            element: {
+              type: "plain_text_input",
+              action_id: "plain_input",
+              placeholder: {
+                type: "plain_text",
+                text: 'e.g. "https://www.denver-restaurants.nl/"',
+              },
+            },
+          },
+        ],
+        submit: {
+          type: "plain_text",
+          text: "Suggest",
+        },
+      },
+    });
+  } catch (error) {
+    logger.error(error);
+  }
 });
 
-app.command("/hi-nextjs", async ({ ack }) => {
-  await ack("Hi there!");
+app.view("view_1", async ({ body, ack, client }: any) => {
+  const { nameInput, locationInput, websiteInput } = body.view.state.values;
+
+  const name: string = nameInput.plain_input.value;
+  const location: string = locationInput.plain_input.value;
+  const website: string = websiteInput.plain_input.value;
+
+  const response = await api.post("/venues", { name, location, website });
+  await api.post("/events", { venueId: response.data._id });
+
+  await client.chat.postMessage({
+    channel: "eat-guild",
+    text: `<@${body.user.id}> suggested to go to <${website}|${name}> in ${location}.`,
+  });
 });
 
 appRunner.setup(app);
