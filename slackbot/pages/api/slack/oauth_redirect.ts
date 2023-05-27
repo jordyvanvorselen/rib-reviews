67bdbfcea95114f1a26a0b0fa97be94a144a7233
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2023-05-27 14:47:06 +0200

    Add oauth redirects

---
+++ slackbot/pages/api/slack/oauth_redirect.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { appRunner } from "./_app";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    res
+      .status(405)
+      .json({ error: "Sorry! This endpoint does not accept your requests." });
+    return;
+  }
+
+  await appRunner.handleCallback(req, res);
+}
