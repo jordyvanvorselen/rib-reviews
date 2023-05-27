77ec00906e40649783686e6648a2669a03745570
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2023-05-27 22:04:46 +0200

    Hotfix slack bot

--- slackbot/pages/api/slack/_app.ts
+++ slackbot/pages/api/slack/_app.ts
@@ -60,11 +60,9 @@ app.view("suggestCallback", async ({ body, ack, client }: any) => {
 
   const { nameInput, locationInput, websiteInput } = body.view.state.values;
 
-  throw new Error(JSON.stringify(body.view.state.values));
-
-  const name: string = nameInput.plain_input.value;
-  const location: string = locationInput.plain_input.value;
-  const website: string = websiteInput.plain_input.value;
+  const name: string = nameInput.input.value;
+  const location: string = locationInput.input.value;
+  const website: string = websiteInput.input.value;
 
   const response = await api.post("/venues", { name, location, website });
   api.post("/events", { venueId: response.data._id });
