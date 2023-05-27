1b074c66de1c5061e36e8e7e456cd383ef87a28e
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2023-05-27 15:06:18 +0200

    Safe accessor for challenge

--- slackbot/pages/api/slack/events.ts
+++ slackbot/pages/api/slack/events.ts
@@ -20,5 +20,5 @@ export default async function handler(
 
   await appRunner.handleEvents(req, res);
 
-  res.status(200).json({ challenge: req.body.challenge });
+  res.status(200).json({ challenge: req.body?.challenge });
 }
